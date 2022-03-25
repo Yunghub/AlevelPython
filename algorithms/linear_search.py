d5073a31dfc59a2a819c90d8cde8fdee706250d8
YungHub <59136907+Yunghub@users.noreply.github.com>
2022-03-25 12:57:54 +0000

    Update linear_search.py

--- algorithms/linear_search.py
+++ algorithms/linear_search.py
@@ -1,5 +1,5 @@
-def linear_search(list):
+def linear_search(list,item):
     for i in range(len(list)):
-        if list[i] == "":
+        if list[i] == item:
             return i
     return len(list)
